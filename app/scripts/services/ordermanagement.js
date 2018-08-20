f66a10b1a61957ce3a273d743d8b3be2b0eedf6c
Sophia Mai <phuongmai001@gmail.com>
2018-08-20 12:13:01 -0700

    update service for order tracking

--- app/scripts/services/ordermanagement.js
+++ app/scripts/services/ordermanagement.js
@@ -10,4 +10,49 @@
 angular.module('udaciMealsApp')
   .service('orderManagement', function () {
     // AngularJS will instantiate a singleton by calling "new" on this function
+    var selectedDay = 'Monday';
+
+    var orderSelection = {
+      Monday: {
+        breakfast: '',
+        lunch: '',
+        dinner: ''
+      },
+      Tuesday: {
+        breakfast: '',
+        lunch: '',
+        dinner: ''
+      },
+      Wednesday: {
+        breakfast: '',
+        lunch: '',
+        dinner: ''
+      },
+      Thursday: {
+        breakfast: '',
+        lunch: '',
+        dinner: ''
+      },
+      Friday: {
+        breakfast: '',
+        lunch: '',
+        dinner: ''
+      }
+    };
+
+    this.getActiveDay = function() {
+      return selectedDay;
+    };
+    this.setActiveDay = function() {
+      selectedDay = day;
+    };
+    this.chooseMenuOption = function(meal, menuItem) {
+      orderSelection[selectedDay][meal] = menuItem;
+    };
+    this.removeMenuOption = function(day, menuCategory) {
+      orderSelection[day][menuCategory] = '';
+    };
+    this.getOrders = function() {
+      return orderSelection;
+    };
   });
