8d53cf7162b3233616ffd55c6c98882269427aae
Sophia Mai <phuongmai001@gmail.com>
2018-08-20 10:24:47 -0700

    update directory

--- app/scripts/services/foodfinder.js
+++ app/scripts/services/foodfinder.js
@@ -10,4 +10,7 @@
 angular.module('udaciMealsApp')
   .service('foodFinder', function () {
     // AngularJS will instantiate a singleton by calling "new" on this function
+    this.getMenu = function() {
+      return $.get('/menu/menu.json');
+    }
   });
